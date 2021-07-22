bc78e69f98784632fbf9a60c099f50d511764f98
Mike <mfmcintyre01@gmail.com>
2021-07-21 22:12:18 -0400

    added Percolation and PercolationStats for week 1

---
+++ src/percolation/Percolation.java
@@ -0,0 +1,94 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    private final boolean[][] grid;
+    private final int gridSize;
+    private int openSites;
+    private final WeightedQuickUnionUF wqf;
+    private final int virtualTop = 0;
+    private final int virtualBottom;
+
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        gridSize = n;
+        openSites = 0;
+        grid = new boolean[n][n];
+        wqf = new WeightedQuickUnionUF(n * n + 2);
+        virtualBottom = n * n + 1;
+    }
+
+    public void open(int row, int col) {
+        if (row > gridSize || col > gridSize) {
+            throw new IllegalArgumentException("Row or Column is too large.");
+        }
+        if (!isOpen(row, col)) {
+            openSites += 1;
+            grid[row - 1][col - 1] = true;
+        }
+
+        if (row == 1) {
+            wqf.union(getNumberIndex(row, col), virtualTop);
+        }
+        if (row == gridSize) {
+            wqf.union(getNumberIndex(row, col), virtualBottom);
+        }
+
+        // union neighbors
+        if (row > 1 && isOpen(row - 1, col)) {
+            wqf.union(getNumberIndex(row, col), getNumberIndex(row - 1, col));
+        }
+        if (row < gridSize && isOpen(row + 1, col)) {
+            wqf.union(getNumberIndex(row, col), getNumberIndex(row + 1, col));
+        }
+        if (col > 1 && isOpen(row, col - 1)) {
+            wqf.union(getNumberIndex(row, col), getNumberIndex(row, col - 1));
+        }
+        if (col < gridSize && isOpen(row, col + 1)) {
+            wqf.union(getNumberIndex(row, col), getNumberIndex(row, col + 1));
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        try {
+            return grid[row - 1][col - 1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException("argument out of range of grid size.");
+        }
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        if (row > 0 && row <= gridSize && col > 0 && col <= gridSize) {
+            return wqf.find(virtualTop) == wqf.find(getNumberIndex(row, col));
+        } else {
+            throw new IllegalArgumentException("argument out of range of grid size.");
+        }
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    private int getNumberIndex(int row, int col) {
+        return gridSize * (row - 1) + col;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return wqf.find(virtualTop) == wqf.find(virtualBottom);
+    }
+
+    public static void main(String[] args) {
+        Percolation p = new Percolation(9);
+        for (int i = 1; i < 10; i++) {
+            p.open(i, 1);
+        }
+        System.out.println(p.numberOfOpenSites());
+        System.out.println(p.percolates());
+    }
+}
---
+++ src/percolation/PercolationStats.java
@@ -0,0 +1,52 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+
+    private final double[] probabilities;
+    private final int numberOfTrials;
+
+    public PercolationStats(int n, int trials) {
+        probabilities = new double[trials];
+        numberOfTrials = trials;
+
+        for (int i = 0; i < trials; i++) {
+            Percolation percolator = new Percolation(n);
+            while (!percolator.percolates()) {
+                int row = StdRandom.uniform(1, n + 1);
+                int col = StdRandom.uniform(1, n + 1);
+                percolator.open(row, col);
+            }
+            probabilities[i] = (double) percolator.numberOfOpenSites() / (n * n);
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(probabilities);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(probabilities);
+    }
+
+    public double confidenceLo() {
+        return mean() - ((1.96 * stddev()) / Math.sqrt(numberOfTrials));
+    }
+
+    public double confidenceHi() {
+        return mean() + ((1.96 * stddev()) / Math.sqrt(numberOfTrials));
+    }
+
+    public static void main(String[] args) {
+        int gridSize = Integer.parseInt(args[0]);
+        int numberOfTrials = Integer.parseInt(args[1]);
+
+        PercolationStats percStats = new PercolationStats(gridSize, numberOfTrials);
+
+        String confidence = "[" + percStats.confidenceLo() + ", " + percStats.confidenceHi() + "]";
+        StdOut.println("mean                    = " + percStats.mean());
+        StdOut.println("stddev                  = " + percStats.stddev());
+        StdOut.println("95% confidence interval = " + confidence);
+    }
+}
